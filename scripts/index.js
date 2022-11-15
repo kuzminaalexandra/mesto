1e2a493bcae73b9beff3f75c8d7eb43736885aa3
kuzminaalexandra <113555701+kuzminaalexandra@users.noreply.github.com>
2022-11-15 22:12:08 +0300

    feat: final version

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,29 @@
 // Проверка работы скрипта
-console.log("Js - это пипец");
+console.log('Js - это пипец');
 
 // Выборка DOM-элементов
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__button-close');
 const popupOpenButtonElement = document.querySelector('.profile__edit-button');
-console.log(popupElement);
-console.log(popupCloseButtonElement);
-console.log(popupOpenButtonElement);
+const formElement = popupElement.querySelector('.popup__content');
+const nameInput = popupElement.querySelector('.popup__name');
+const professionInput = popupElement.querySelector('.popup__profession');
+const profileName = document.querySelector('.profile__title');
+const profileProfession = document.querySelector('.profile__subtitle');
 
-
-// const togglePopupVisibility = function () {
-//     popupElement.classList.toggle('popup__is-opened');
-// }
-
-const openPopup = function (event) {
+// Открытие попапа
+const openPopup = function () {
     popupElement.classList.add('popup__is-opened');
-    console.log('Open popup clicked');
+    nameInput.value = profileName.textContent;
+    professionInput.value = profileProfession.textContent;
 }
 
+// Закрытие попапа
 const closePopup = function () {
     popupElement.classList.remove('popup__is-opened');
 }
 
+// Закрытие попапа при клике на пространстве за рамками попапа
 const closePopupByClickOnOverlay = function (event) {
     console.log(event.target, event.currentTarget);
     if (event.target !== event.currentTarget) {
@@ -31,22 +32,15 @@ const closePopupByClickOnOverlay = function (event) {
     closePopup();
 }
 
-// togglePopupVisibility();
-
-// Регистрация обработчика событий по клику
+//Обработка отправки введенных в попап данных
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileProfession.textContent = professionInput.value;
+    closePopup();
+}
 
 popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
-popupElement.addEventListener('click', closePopupByClickOnOverlay)
-
-let formElement = popupElement.querySelector('.popup__container');
-let nameInput = popupElement.querySelector('.popup__name');
-let jobInput = popupElement.querySelector('.popup__profession');
-
-//Обработка отправки формы
-function formSubmitHandler(evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы. И происходит какая-то дикая херота....
-}
-// Получение значений полей jobInput и nameInput из свойства value
-
-
+popupElement.addEventListener('click', closePopupByClickOnOverlay);
+formElement.addEventListener('submit', formSubmitHandler);
