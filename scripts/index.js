afed1a55a6fc2576607b2639a8c1f6d8dbbb3e9b
kuzminaalexandra <113555701+kuzminaalexandra@users.noreply.github.com>
2022-11-28 20:47:14 +0300

    feat: сломала нафиг весь код ~ಠ益ಠ~

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,72 @@
 // Проверка работы скрипта
-console.log('Js - это не пипец) Учитель говорит, у меня отлично получается)) Александр, спасибо за ревью!');
+console.log('JS - это боль ಠ_ಠ');
+
+// Создание массива
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
 
 // Выборка DOM-элементов
 const popupElement = document.querySelector('.popup');
+const popupPhotoElement = document.querySelector('#popup-photo-card');
 const popupCloseButtonElement = popupElement.querySelector('.popup__button-close');
+const popupCloseAddButtonElement = popupElement.querySelector('.popup__button-close');
 const popupOpenButtonElement = document.querySelector('.profile__edit-button');
+const popupAddButtonElement = document.querySelector('.profile__add-button');
 const formElement = popupElement.querySelector('.popup__content');
 const nameInput = popupElement.querySelector('.popup__name');
 const professionInput = popupElement.querySelector('.popup__profession');
 const profileName = document.querySelector('.profile__title');
 const profileProfession = document.querySelector('.profile__subtitle');
 
-// Открытие попапа
+const photoCardsContainer = document.querySelector('.elements__container');
+const addTemplate = document.querySelector('#template__elements').content.querySelector('.elements__photo');
+
+// Открытие попапа редактирования профиля
 const openPopup = function () {
     popupElement.classList.add('popup__is-opened');
     nameInput.value = profileName.textContent;
     professionInput.value = profileProfession.textContent;
 }
 
+popupOpenButtonElement.addEventListener('click', openPopup);
+
+// Открытие попапа редактирования фото-карточки
+const openPopupPhotoCard = function () {
+    popupPhotoElement.classList.add('popup__is-opened');
+}
+
+popupAddButtonElement.addEventListener('click', openPopupPhotoCard);
+
 // Закрытие попапа
 const closePopup = function () {
     popupElement.classList.remove('popup__is-opened');
 }
+
+popupCloseButtonElement.addEventListener('click', closePopup);
 
 // Закрытие попапа при клике на пространстве за рамками попапа
 const closePopupByClickOnOverlay = function (event) {
@@ -29,6 +74,8 @@ const closePopupByClickOnOverlay = function (event) {
         closePopup();
     }
 }
+
+popupElement.addEventListener('click', closePopupByClickOnOverlay);
 
 //Обработка отправки введенных в попап данных
 function formSubmitHandler(evt) {
@@ -38,8 +85,25 @@ function formSubmitHandler(evt) {
     closePopup();
 }
 
-//Подключение слушателей
-popupOpenButtonElement.addEventListener('click', openPopup);
-popupCloseButtonElement.addEventListener('click', closePopup);
-popupElement.addEventListener('click', closePopupByClickOnOverlay);
-formElement.addEventListener('submit', formSubmitHandler);
+initialCards.forEach((dataCard) => {
+renderCard(dataCard);
+});
+
+//Функция создания фото-контейнера
+const generateCard = (dataCard) => {
+    const addPhotoContainer = addTemplate.cloneNode(true);
+    const addPhoto = addPhotoContainer.querySelector('.elements__photo-card'); 
+    const addPhotoName = addPhotoContainer.querySelector('.elements__paragraph');
+
+    addPhoto.src = item.link;
+    addPhotoName.textContent = item.name;
+
+    return addPhotoContainer;
+}
+
+//Добавление карточки
+const renderCard = (dataCard) => {
+    photoCardsContainer.prepend(generateCard(dataCard))
+};
+
+
